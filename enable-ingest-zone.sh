bfd2692dd7f58082422450aad6670c886de1d261
Paul van Schayck <polleke@gmail.com>
2015-09-10 10:24:30 +0200

    Initial version of helper commands to be executed from msiExecCmd()

---
+++ enable-ingest-zone.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Enables an ingest zone for a certain user by setting the cifs acl. First obtain the sid
+
+sid=$($(dirname "$0")/name-to-sid.py $1)
+
+if [ -z "$sid" ]; then
+	exit 1
+fi
+
+# Set all rights except special permissions and full control, also let them inherit
+# Versions of setcifsacl that I tested would always return a non zero return code, even on success
+/usr/sbin/setcifsacl -a "ACL:${sid}:ALLOWED/OI|CI/CHANGE" $2
+
+exit 0
---
+++ name-to-sid.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+import ldap
+import struct
+import string
+import sys
+
+def strsid(byte):
+    '''
+    Convert bytes into a string SID
+    byte - bytes to convert
+    '''
+    ret = 'S'
+    sid = []
+    sid.append(byteToLong(byte[0]))
+    sid.append(byteToLong(byte[2:2+6], False))
+    for i in range(8, len(byte), 4):
+        sid.append(byteToLong(byte[i:i+4]))
+    for i in sid:
+        ret += '-' + str(i)
+    return ret
+
+def byteToLong(byte, little_endian=True):
+    '''
+    Convert bytes into a Python integer
+    byte - bytes to convert
+    little_endian - True (default) or False for little or big endian
+    '''
+    if len(byte) > 8:
+        raise Exception('Bytes too long. Needs to be <= 8 or 64bit')
+    else:
+        if little_endian:
+            a = string.ljust(byte, 8, '\x00')
+            return struct.unpack('<q', a)[0]
+        else:
+            a = string.rjust(byte, 8, '\x00')
+            return struct.unpack('>q', a)[0] 
+
+def readCifsCredentials():
+    with open("/etc/cifs-credentials") as f:
+        for line in f:          
+            if line.startswith('password='):
+                password = line[line.index('=') + 1:]
+            if line.startswith('username='):
+                username = line[line.index('=') + 1:]
+    
+    return (password.strip(), username.strip())
+
+if len(sys.argv) == 1:
+    sys.stderr.write("name-to-sid.py: Supply user as first argument \n")
+    sys.exit(1)
+
+password, username = readCifsCredentials()
+
+l = ldap.initialize('ldap://ldap.maastrichtuniversity.nl')
+
+l.protocol_version = ldap.VERSION3
+l.simple_bind_s("CN=%s (M4I),OU=M4I-Nanoscopy-Resource,OU=Users,OU=M4I,OU=FHML,DC=unimaas,DC=nl" % username, password)
+
+baseDN             = 'DC=unimaas,DC=nl'
+searchScope        = ldap.SCOPE_SUBTREE
+retrieveAttributes = ['objectSid']
+searchFilter       = "sAMAccountName=%s" % sys.argv[1]
+
+id = l.search(baseDN, searchScope, searchFilter, retrieveAttributes)
+
+result_type, result = l.result(id, 0)
+
+if not result_type == ldap.RES_SEARCH_ENTRY:
+    sys.stderr.write("name-to-sid.py: User not found \n")
+    sys.exit(1)
+
+rawSid = result[0][1]['objectSid'][0]
+
+print strsid(rawSid)
