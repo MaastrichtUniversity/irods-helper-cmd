a1bd2d42f93e07256588deec2405d25256108e97
Paul van Schayck <polleke@gmail.com>
2018-04-18 11:53:58 +0200

    First set permissions of ingest zone to 755, before deleting. Then force delete. RITDEV-413

--- disable-ingest-zone.sh
+++ disable-ingest-zone.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-# Disables the ingest zone by removing is
+# Break on error
+set -e
 
-rm -r $1
+# First fix permissions so that we can always delete the files in a drop zone, in case files were made read-only
+chmod -R 755 $1
+
+# Then delete its contents and the directory
+rm -rf $1
 
 exit 0
